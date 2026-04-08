f6c446bee583a35c7d345e97e7e578bf5c3d4db4
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-08 16:22:12 +0700

    app/data.model: added AuthState

--- app/src/main/java/com/example/notesapp/data/model/AuthState.kt
+++ app/src/main/java/com/example/notesapp/data/model/AuthState.kt
@@ -1,2 +1,19 @@
 package com.example.notesapp.data.model
 
+/**
+ * Represent the current authentication state.
+ * Using a sealed class ensure we handle all possible states
+ */
+sealed class AuthState {
+    // Initialize state - user not logged in, no action progress
+    object Idle: AuthState()
+
+    // Loading state - login/register in progress
+    object Loading: AuthState()
+
+    // Success state - user is authenticated
+    object Authenticated: AuthState()
+
+    // Error state: - something went wrong
+    data class Error(val message: String) : AuthState()
+}
