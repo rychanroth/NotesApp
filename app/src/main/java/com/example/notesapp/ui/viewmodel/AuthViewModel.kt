a1bd1fec36a5f911172b7235b393a3b58c54bd1e
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-11 18:05:54 +0700

    app/ui.viewmodel/AuthViewModel.kt added

---
+++ app/src/main/java/com/example/notesapp/ui/viewmodel/AuthViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.notesapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notesapp.data.model.AuthState
+import com.example.notesapp.data.repository.AuthRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class AuthViewModel(
+     private val repository: AuthRepository = AuthRepository()
+): ViewModel() {
+    // Private mutable state
+    private val _authState = MutableStateFlow<AuthState>(AuthState.Idle)
+
+    // Public immutable state (read-only for UI)
+    val authState: StateFlow<AuthState> = _authState.asStateFlow()
+
+    init {
+        // Check if user is already logged in
+        if (repository.isLoggedIn) {
+            _authState.value = AuthState.Authenticated
+        }
+    }
+
+    /**
+     * Login with email and password.
+     */
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            _authState.value = AuthState.Loading
+
+            when (val result = repository.login(email, password)) {
+                is com.example.notesapp.data.model.Result.Success -> {
+                    _authState.value = AuthState.Authenticated
+                }
+                is com.example.notesapp.data.model.Result.Error -> {
+                    _authState.value = AuthState.Error (
+                        result.exception.message ?: "Registration failed"
+                    )
+                }
+            }
+        }
+    }
+
+    /**
+     * Send password reset email.
+     */
+    fun sendPasswordReset(email: String, onResult: (Boolean, String) -> Unit) {
+        viewModelScope.launch {
+            when (val result = repository.sendPasswordResetEmail(email)) {
+                is com.example.notesapp.data.model.Result.Success -> {
+                    onResult(true, "Password reset email sent!")
+                }
+                is com.example.notesapp.data.model.Result.Error -> {
+                    onResult(false, result.exception.message ?: "Failed to send reset email.")
+                }
+            }
+        }
+    }
+
+    /**
+     * Logout of the current user.
+     */
+    fun logout() {
+        repository.logout()
+        _authState.value = AuthState.Idle
+    }
+}
